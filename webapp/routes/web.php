8b04af95a16590c4eb3d5c4dc6d2530add7e81d4
namvfg <duakhongchau1234@gmail.com>
2025-12-30 20:34:14 +0700

    add layout

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, "index"])->name("home");
+
+Route::get("/signup", [SignupController::class, "create"]);
